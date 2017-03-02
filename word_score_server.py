b39f4531f047ee6d1b52d03d0b79945d7d53b436
Yusuf Musleh <yusuf.musleh@gmail.com>
2017-03-01 20:22:07 -0500

    fixed bug with mixed case words

--- word_score_server.py
+++ word_score_server.py
@@ -8,15 +8,15 @@ word_score_server = Flask(__name__)
 def get_word_score(word):
 
 	# source: http://www.wordfind.com/scrabble-letter-values/
-	letter_score_map = {'a': 1, 'b': 3, 'c': 3,
-						'd': 2, 'e': 1, 'f': 4,
-						'g': 2, 'h': 4, 'i': 1,
-						'j': 8, 'k': 5, 'l': 1,
-						'm': 3, 'n': 1, 'o': 1,
-						'p': 3, 'q': 10, 'r': 1,
-						's': 1, 't': 1, 'u': 1,
-						'v': 4, 'w': 4,	'x': 8,
-						'y': 4, 'z': 10}
+	letter_score_map = {'A': 1, 'B': 3, 'C': 3,
+						'D': 2, 'E': 1, 'F': 4,
+						'G': 2, 'H': 4, 'I': 1,
+						'J': 8, 'K': 5, 'L': 1,
+						'M': 3, 'N': 1, 'O': 1,
+						'P': 3, 'Q': 10, 'R': 1,
+						'S': 1, 'T': 1, 'U': 1,
+						'V': 4, 'W': 4,	'X': 8,
+						'Y': 4, 'Z': 10}
 
 	score = 0
 	for letter in word:
@@ -32,7 +32,7 @@ def is_valid_scrabble_word(word):
 	# source: http://stackoverflow.com/questions/4940032/search-for-string-in-txt-file-python
 	with open("acceptable_words.txt", "r+b") as f:
 		s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
-		if s.find(word.upper()) != -1:
+		if s.find(word) != -1:
 			return True
 		return False
 
@@ -47,8 +47,9 @@ def not_found(error):
 @word_score_server.route('/wordscore/<string:word>', methods=['GET'])
 def wordscore(word):
 	try:
-		if is_valid_scrabble_word(word):
-			return jsonify({'word': word, 'valid': True, 'score': get_word_score(word)})
+		uppercase_word = word.upper()
+		if is_valid_scrabble_word(uppercase_word):
+			return jsonify({'word': word, 'valid': True, 'score': get_word_score(uppercase_word)})
 		else:
 			return jsonify({'word': word, 'valid': False})
 	except:
